97bc8c7b0c0b4aba116dc01f9c3acb9363e52d52
JSP <realitycheck@users.noreply.github.com>
2022-06-15 13:11:25 +0300

    delay added

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/micmonay/keybd_event"
@@ -12,13 +14,35 @@ func main() {
 	k := flag.Int("k", 0x5B+0xFFF, "key") // LWIN
 	t := flag.Int("t", 3600, "seconds")
 	linux := flag.Bool("l", false, "linux")
+	delay := flag.Duration("d", 0, "delay")
+	quiet := flag.Bool("q", false, "quiet")
+	noDelay := flag.Bool("n", false, "no delay")
 	flag.Parse()
 
-	fmt.Printf("Pressing %d for %d seconds...\n", *k, *t)
+	if *quiet {
+		log.SetOutput(io.Discard)
+		log.SetFlags(0)
+	}
+
+	if *delay == 0 && !*noDelay {
+		var s string
+		var err error
+		fmt.Print("delay: ")
+		fmt.Scanf("%s", &s)
+		*delay, err = time.ParseDuration(s)
+		if err != nil && len(s) > 0 {
+			log.Print(err)
+		}
+	}
+
+	if *delay != 0 {
+		log.Printf("Delay for %.2f seconds.", delay.Seconds())
+		time.Sleep(*delay)
+	}
 
 	kb, err := keybd_event.NewKeyBonding()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if *linux {
@@ -29,13 +53,15 @@ func main() {
 
 	defer kb.Release()
 
+	log.Printf("Pressing %d for %d seconds...\n", *k, *t)
+
 	for i := 0; i < *t; i++ {
 		if err := kb.Press(); err != nil {
-			println(err)
+			log.Print(err)
 		}
 		time.Sleep(500 * time.Millisecond)
 		if err := kb.Release(); err != nil {
-			println(err)
+			log.Print(err)
 		}
 		time.Sleep(500 * time.Millisecond)
 	}
